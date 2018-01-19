960059de0b9e1a971aba5b4aa8bc6f14a903dff8
talhassid <talhassidd@gmail.com>
2018-01-19 16:53:09 +0200

    rearrange files

--- q3.py
+++ q3.py
@@ -1,33 +1,13 @@
-import csv
-from typing import List
-
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
-
-def load_data(path: str) -> List[int]:
-    """
-    Load the dataset and return as a list
-
-    :param path:  path to the flare.csv
-    :return: the features per example and it's label
-    """
-    with open(path, 'r') as f:
-        data = list(csv.reader(f, delimiter=","))
-        for e in range(1, len(data[1:]) + 1):
-            for f in range(len(data[e]) - 1):
-                data[e][f] = int(data[e][f])
-            data[e][-1] = (data[e][-1] == "True")
-    return data[1:]
-
+from data import load_data
 
 if __name__ == '__main__':
     path = "./flare.csv"
-    data = load_data(path)
-    data_ = [row[:-1] for row in data[1:]]
-    labels_ = [row[-1] for row in data[1:]]
+    data, labels = load_data(path)
 
-    x_train, x_test, y_train, y_test = train_test_split(data_, labels_, test_size=0.25)
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.25)
     clf_overfitting = DecisionTreeClassifier(criterion="entropy", min_samples_leaf=1, min_samples_split=2)
     clf_overfitting.fit(X=x_train, y=y_train)
     print(clf_overfitting.score(X=x_train, y=y_train))
